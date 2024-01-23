02db2a4b84ade011a864604f1d699900a273a24e
abrik1 <133798548+abrik1@users.noreply.github.com>
2024-01-23 23:15:30 +0530

    database in lua

---
+++ db.lua
@@ -0,0 +1,27 @@
+pkgs = {
+    firefox = {
+        version = "117.0.1",
+        description = "Standalone web browser from mozilla.org",
+        licence = "free"
+    },
+    hew = {
+        version = "0.1",
+        description = "A simple program printing hello world",
+        licence = "free"
+    },
+    maxfetch = {
+        version = "1.2",
+        description = "A fetching program written in shell by jobcmax",
+        licence = "free"
+    },
+    neofetch = {
+        version = "7.1.0",
+        description = "A CLI system information tool written in BASH that supports displaying images.",
+        licence = "free"
+    },
+    choosenim = {
+        version = "0.8.4",
+        description = "Tool for easily installing and managing multiple versions of the Nim programming language.",
+        licence = "free"
+    }
+}
